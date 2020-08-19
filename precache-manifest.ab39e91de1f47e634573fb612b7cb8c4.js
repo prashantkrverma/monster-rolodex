b1677c08b4bae77cd59b54293f2c032d63f3f73f
Prashant Kumar Verma <prashantkrverma51@gmail.com>
2020-08-20 00:27:04 +0530

    Updates

---
+++ precache-manifest.ab39e91de1f47e634573fb612b7cb8c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360e51eefc60d2bb1895df35d74d8d7d",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f72b644548ca9cd21df9",
+    "url": "/monster-rolodex/static/css/main.d5c999be.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f72b644548ca9cd21df9",
+    "url": "/monster-rolodex/static/js/main.2400bd91.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
